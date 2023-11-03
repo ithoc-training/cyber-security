7ed052c8486efdfa1ec15b1b76005f5c3f10cecc
Oli Hock <oliver.hock@gmail.com>
2023-11-03 10:28:29 +0100

    Refactor server and client to include CSRF protection.

--- csrf/csrf_client.py
+++ csrf/csrf_client.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template_string
+import requests
 
 app = Flask(__name__)
 
@@ -7,19 +8,27 @@ app = Flask(__name__)
 def index():
     # Eine einfache HTML-Form ohne CSRF-Token
     return render_template_string('''
-    <form action="/change-email" method="POST">
+    <form action="/change_email" method="POST">
       <input type="email" name="email" value="" placeholder="Enter new email" />
       <input type="submit" value="Change Email" />
     </form>
     ''')
 
 
-@app.route('/change-email', methods=['POST'])
+@app.route('/change_email', methods=['POST'])
 def change_email():
     email = request.form['email']
     # Hier würden Sie die E-Mail im Benutzerprofil aktualisieren
     # Aber da es keinen CSRF-Schutz gibt, könnte jede Seite diese Aktion ausführen
-    return 'E-Mail wurde geändert zu: ' + email
+
+    token_response = requests.post('http://localhost:5006/token', json={'user_id': 'user1'})
+    user_token = token_response.json()['user_token']
+
+    email_response = requests.put('http://localhost:5006/email/' + email, json={'user_token': user_token})
+    if email_response.status_code == 401:
+        return 'Nicht autorisiert'
+    else:
+        return 'E-Mail wurde geändert zu: ' + email
 
 
 if __name__ == '__main__':
--- csrf/csrf_server.py
+++ csrf/csrf_server.py
@@ -1,8 +1,11 @@
-from flask import Flask, Response, jsonify
+import uuid
+from flask import Flask, Response, jsonify, request
 
 app = Flask(__name__)
 
 profile_email = {"email": "oli@example.com"}
+
+session_store: [] = []
 
 
 @app.route('/email')
@@ -10,10 +13,23 @@ def read_email():
     return jsonify({"email": profile_email['email']})
 
 
+@app.route('/token', methods=['POST'])
+def token():
+    body = request.get_json()
+    user_id: str = body['user_id']
+    user_token: str = str(uuid.uuid1())
+    session_store.append(user_token)
+    return jsonify({"user_token": user_token})
+
+
 @app.route('/email/<new_email>', methods=['PUT'])
 def change_email(new_email):
-    profile_email['email'] = new_email
-    return Response(status=200)
+    user_token = request.json['user_token']
+    if user_token in session_store:
+        profile_email['email'] = new_email
+        return Response(status=200)
+    else:
+        return Response(status=401)
 
 
 if __name__ == '__main__':
